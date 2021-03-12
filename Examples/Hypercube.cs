e8050ce800c13809081de8d7dca7c2374b4b5b18
Hiroyuki Inou <inou@math.kyoto-u.ac.jp>
2021-03-12 15:42:39 +0900

    Update examples (hypercube & RP2)

--- Examples/Hypercube.cs
+++ Examples/Hypercube.cs
@@ -7,9 +7,87 @@ namespace Pcx4D
 {
     public class Hypercube : MonoBehaviour
     {
-        [SerializeField] int numPoints = 100;
+        [SerializeField] int numPoints = 1000;
         [SerializeField] float offset = 0f;
+        public int dimension = 1;
+        public bool chiral = false;
+
+        [SerializeField] Color color1 = new Color(0.75f, 0, 0);
+        [SerializeField] Color color2 = new Color(0, 0.75f, 0);
+        [SerializeField] Color color3 = new Color(0, 0, 0.75f);
+        [SerializeField] Color color4 = new Color(0.25f, 0.25f, 0.25f);
         
+
+
+        List<List<int>> combinations = new List<List<int>>();
+        
+        void InitCombination(int n=4)
+        {            
+            for (int k=0; k<=n; k++)
+            {
+                combinations.Add(new List<int>());
+            }
+
+            for (int i=0; i< (1<<n); i++)
+            {
+                int count = 0;
+                int j = i;
+                while (j>0)
+                {
+                    count += j & 1;
+                    j >>= 1;
+                }
+                combinations[count].Add(i);
+            }
+        }
+
+        // random distribution in the specified dimensional cells
+        Mesh CreateMesh2( int n )
+        {
+            if (dimension < 0) dimension = 0;
+            if (dimension > 4) dimension = 4;
+
+            List<Vector3> vs = new List<Vector3>();
+            List<Vector2> uvs = new List<Vector2>();
+            List<Color> cols = new List<Color>();
+
+            if (combinations.Count > 0)
+            {
+                foreach (var l in combinations[dimension])
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        Vector4 v = new Vector4();
+                        for (int k = 0; k < 4; k++)
+                        {
+                            if ((l & (1 << k)) > 0)
+                            {
+                                v[k] = Random.value * 2 - 1;
+                            }
+                            else
+                            {
+                                v[k] = (Random.value < 0.5) ? -1 : 1;
+                            }
+                        }
+                        vs.Add(new Vector3(v.x, v.y, v.z));
+                        uvs.Add(new Vector2((chiral ? -1 : 1) * v.w, 0));
+                        cols.Add((v.x + 1) / 2 * color1 + (v.y + 1) / 2 * color2 + (v.z + 1) / 2 * color3 + (v.w + 1) / 2 * color4);
+                    }
+                }
+            }
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(vs);
+            mesh.SetUVs(1, uvs);
+            mesh.SetColors(cols);
+            mesh.SetIndices(
+                    Enumerable.Range(0, vs.Count).ToArray(),
+                    MeshTopology.Points, 0
+                );
+
+            return mesh;
+        }
+
+        // points on edges (equal interval)
         Mesh CreateMesh(int n)
         {
             List<Vector3> vs = new List<Vector3>();
@@ -56,20 +134,29 @@ namespace Pcx4D
 
         void SetMesh()
         {
-            Mesh mesh = CreateMesh(numPoints);
+            Mesh mesh = CreateMesh2(numPoints);
             gameObject.GetComponent<MeshFilter>().mesh = mesh;
         }
 
-        // Start is called before the first frame update
-        void Start()
+        void Awake()
         {
+            InitCombination();
             SetMesh();
+            Debug.Log("Hypercube: initialize mesh.");
         }
 
         // Update is called once per frame
         void OnValidate()
         {
-            SetMesh();
+            if (Application.isPlaying)
+            {
+                SetMesh();
+            } else
+            {
+                // reduce the size of the scene
+                gameObject.GetComponent<MeshFilter>().mesh = null;
+            }
         }
+
     }
 }
---
+++ Examples/RP2.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Pcx4D
+{
+    public class RP2 : MonoBehaviour
+    {
+        [SerializeField] int numPoints = 256;
+        [SerializeField] float offset = 0f;
+        public bool chiral = false;
+
+        // convert an element in S^2 to RP^2
+        // (x,y,z) -> (xy, xz, y^2-z^2, 2yz)
+        Vector4 S2toRP2(Vector3 t)
+        {
+            return new Vector4(t.x * t.y, t.x * t.z, t.y * t.y - t.z * t.z, 2 * t.y * t.z);
+        }
+
+        // random distribution in the specified dimensional cells
+        Mesh CreateMesh2(int n)
+        {
+            List<Vector3> vs = new List<Vector3>();
+            List<Vector2> uvs = new List<Vector2>();
+            List<Color> cols = new List<Color>();
+
+            for (int i = 0; i < n; i++) {
+                Vector2 v = new Vector2(Random.value, Random.value);
+                cols.Add(new Color(v.x, v.y, 1));
+                v *= Mathf.PI;
+                Vector3 t = new Vector3(Mathf.Cos(v.x)*Mathf.Sin(v.y), Mathf.Sin(v.x)*Mathf.Sin(v.y),Mathf.Cos(v.y));
+                Vector4 q = S2toRP2(t);
+
+                vs.Add(new Vector3(q.x, q.y, q.z));
+                uvs.Add(new Vector2((chiral ? -1 : 1) * q.w, 0));
+            }
+
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(vs);
+            mesh.SetUVs(1, uvs);
+            mesh.SetColors(cols);
+            mesh.SetIndices(
+                    Enumerable.Range(0, vs.Count).ToArray(),
+                    MeshTopology.Points, 0
+                );
+
+            return mesh;
+        }
+
+        // points on edges (equal interval)
+        Mesh CreateMesh(int n)
+        {
+            List<Vector3> vs = new List<Vector3>();
+            List<Vector2> uvs = new List<Vector2>();
+            List<Color> cols = new List<Color>();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    float x = i * Mathf.PI / n, y = j * Mathf.PI / n;
+                    Vector3 t = new Vector3(Mathf.Cos(x) * Mathf.Sin(y), Mathf.Sin(x) * Mathf.Sin(y), Mathf.Cos(y));
+                    Vector4 q = S2toRP2(t);
+
+                    if (chiral) { q.w = -q.w; }
+
+                    Color col = new Color((float)i / n, (float)j / n, 1);
+
+                    if ( ((i % 8) == 0) || ( (j % 8) == 0) )
+                    {
+                        col = Color.white;
+                    }
+
+                    vs.Add(new Vector3(q.x, q.y, q.z));
+                    uvs.Add(new Vector2(q.w, 0f));
+                    cols.Add(col);
+                 
+                }
+            }
+            
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(vs);
+            mesh.SetUVs(1, uvs);
+            mesh.SetColors(cols);
+            mesh.SetIndices(
+                    Enumerable.Range(0, vs.Count).ToArray(),
+                    MeshTopology.Points, 0
+                );
+
+            return mesh;
+        }
+
+        void SetMesh()
+        {
+            Mesh mesh = CreateMesh(numPoints);
+            gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        }
+
+        void Awake()
+        {
+            SetMesh();
+        }
+
+        // Update is called once per frame
+        void OnValidate()
+        {
+            if (Application.isPlaying)
+            {
+                SetMesh();
+            } else
+            {
+                // reduce the size of the scene
+                gameObject.GetComponent<MeshFilter>().mesh = null;
+            }
+        }
+
+    }
+}
