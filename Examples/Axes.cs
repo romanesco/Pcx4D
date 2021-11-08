e1a8cead3f17c7a8934d5bc15c3b5e8ff061693a
Hiroyuki Inou <inou@math.kyoto-u.ac.jp>
2021-11-08 13:34:34 +0900

    Enable to change lengths and colors of the axes

--- Examples/Axes.cs
+++ Examples/Axes.cs
@@ -8,7 +8,16 @@ namespace Pcx4D
     public class Axes : MonoBehaviour
     {
         [SerializeField] int numPoints = 100;
-        
+
+        [SerializeField] Color color1 = Color.HSVToRGB(0, 1, 1);
+        [SerializeField] Color color2 = Color.HSVToRGB(0.25f, 1, 1);
+        [SerializeField] Color color3 = Color.HSVToRGB(0.5f, 1, 1);
+        [SerializeField] Color color4 = Color.HSVToRGB(0.75f, 1, 1);
+
+        Color[] colors = new Color[4];
+
+        [SerializeField] Vector4 length = new Vector4(1, 1, 1, 1);
+
         Mesh CreateMesh(int n)
         {
             List<Vector3> vs = new List<Vector3>();
@@ -17,17 +26,16 @@ namespace Pcx4D
 
             for (int i=0; i<4; i++)
             {
-                Color col = Color.HSVToRGB(1f / 4 * i, 1, 1);
                 Vector4 p = new Vector4(0,0,0,0);
                 p[i] = 1;
-                for ( int l = 0; l < n; l++ )
+                for ( int l = 0; l < n*length[i]; l++ )
                 {
                     float t = 1f / (n - 1) * l;
                     Vector4 q = t * p;
 
                     vs.Add(new Vector3(q.x, q.y, q.z));
                     uvs.Add(new Vector2(q.w, 0f));
-                    cols.Add(col);
+                    cols.Add(colors[i]);
                 }
             }
             
@@ -55,6 +63,19 @@ namespace Pcx4D
             }
         }
 
+        private void Start()
+        {
+            SetColors();
+        }
+
+        void SetColors()
+        {
+            colors[0] = color1;
+            colors[1] = color2;
+            colors[2] = color3;
+            colors[3] = color4;
+        }
+
         void Awake()
         {
             SetMesh();
@@ -63,6 +84,8 @@ namespace Pcx4D
         // Update is called once per frame
         void OnValidate()
         {
+            SetColors();
+
             if (Application.isPlaying)
             {
                 SetMesh();
