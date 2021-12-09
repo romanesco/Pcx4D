2a8c3e7b4cfe09a7b5214a24b93644b4fc86d2f7
romanesco <10074398+romanesco@users.noreply.github.com>
2021-12-09 18:13:03 +0900

    Rotate any direction and automatic rotation

---
+++ Scripts/AutoRotate4D.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pcx4D {
+    public class AutoRotate4D : MonoBehaviour
+    {
+        public float period = 5;
+        [SerializeField] bool assignRandomRotationAtStart = true;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (assignRandomRotationAtStart)
+            {
+                GetComponent<Rotate4D>().rotation4D = RandomRotation.randomDistributionOnSO4();
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Rotate4D rotate4D = GetComponent<Rotate4D>();
+            if (period <= 0) period = 5;
+            rotate4D.period = period;
+            int j = (int)(Time.time / period) % 6;
+            Debug.Log(Time.time);
+            Debug.Log(Time.time/period);
+            Debug.Log(j);
+            for (int i = 0; i < 6; i++)
+            {
+                rotate4D.angles[i] = 0;
+            }
+            rotate4D.angles[j] = 1;
+        }
+    }
+
+}
--- Scripts/Rotate4D.cs
+++ Scripts/Rotate4D.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace Pcx4D
 {
     public class Rotate4D : MonoBehaviour
     {
 
-        [SerializeField] float period = 5;
+        public float period = 5;
         [SerializeField] bool stopOnDisable = true;
+
+        public Matrix4x4 rotation4D= Matrix4x4.identity;
+
+        public float[] angles = new float[] { 1, 0, 0, 0, 0, 1 };
 
         float offset = 0;
         float disableTime = 0;
@@ -23,8 +28,23 @@ namespace Pcx4D
             }
         }
 
+        Matrix4x4 MatrixExponential(Matrix4x4 m)
+        {
+            const int deg = 5;
+            Matrix4x4 A = Matrix4x4.identity, B = Matrix4x4.identity;
+            int j = 1;
+            for (int i=1; i<deg; i++)
+            {
+                j *= i;
+                B *= m;
+                A += 1.0f / j * (Unity.Mathematics.float4x4) B;
+            }
+            // need to make A orthogonal?
+            return A;
+        }
+
         // Update is called once per frame
-        void Update()
+        void OldUpdate ()
         {
             float t = (Time.time - offset) * 2 * Mathf.PI / period,
                 c = Mathf.Cos(t), s = Mathf.Sin(t);
@@ -32,10 +52,49 @@ namespace Pcx4D
             GetComponent<Renderer>().sharedMaterial.SetMatrix("_Rotation4D", _rotation4D);
         }
 
+        void OldUpdate2()
+        {
+            float t = Time.deltaTime * 2 * Mathf.PI / period,
+                c = Mathf.Cos(t), s = Mathf.Sin(t);
+            Matrix4x4 deltaRotation4D = new Matrix4x4(new Vector4(c, s, 0, 0), new Vector4(-s, c, 0, 0), new Vector4(0, 0, c, -s), new Vector4(0, 0, s, c));
+            rotation4D = deltaRotation4D * rotation4D;
+            GetComponent<Renderer>().sharedMaterial.SetMatrix("_Rotation4D", rotation4D);
+        }
+
+        private void Update()
+        {
+            Matrix4x4 B = Matrix4x4.zero;
+            float t = Time.deltaTime * 2 * Mathf.PI/period;
+            B[0, 1] = t * angles[0];
+            B[0, 2] = t * angles[1];
+            B[0, 3] = t * angles[2];
+            B[1, 2] = t * angles [3];
+            B[1, 3] = t * angles [4];
+            B[2, 3] = t * angles [5];
+
+            B[1, 0] = -t * angles[0];
+            B[2, 0] = -t * angles[1];
+            B[3, 0] = -t * angles[2];
+            B[2, 1] = -t * angles[3];
+            B[3, 1] = -t * angles[4];
+            B[3, 2] = -t * angles[5];
+
+            rotation4D = MatrixExponential(B) * rotation4D;
+            GetComponent<Renderer>().sharedMaterial.SetMatrix("_Rotation4D", rotation4D);
+        }
+
         private void OnDisable()
         {
             disableTime = Time.time;
         }
 
+        void OnValidate()
+        {
+            if (angles.Length != 6)
+            {
+                Debug.LogWarning("Don't change the 'angles' field's array size!");
+                System.Array.Resize(ref angles, 6);
+            }
+        }
     }
 }
