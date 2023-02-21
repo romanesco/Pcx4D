89b9573bb1e9a6de3b8ef157b28e575151ebbf11
Hiroyuki Inou <inou@math.kyoto-u.ac.jp>
2023-02-21 11:42:13 +0900

    Add restricted random rotations

--- Scripts/RandomRotation.cs
+++ Scripts/RandomRotation.cs
@@ -6,6 +6,45 @@ namespace Pcx4D
 {
     public static class RandomRotation
     {
+        public static Matrix4x4 randomDistributionOnSO3()
+        {
+            Quaternion q1 = Random.rotation;
+            Matrix4x4 A1 = new Matrix4x4();
+            A1[0, 0] = q1.x;
+            A1[0, 1] = -q1.y;
+            A1[0, 2] = -q1.z;
+            A1[0, 3] = -q1.w;
+            A1[1, 0] = q1.y;
+            A1[1, 1] = q1.x;
+            A1[1, 2] = -q1.w;
+            A1[1, 3] = q1.z;
+            A1[2, 0] = q1.z;
+            A1[2, 1] = q1.w;
+            A1[2, 2] = q1.x;
+            A1[2, 3] = -q1.y;
+            A1[3, 0] = q1.w;
+            A1[3, 1] = -q1.z;
+            A1[3, 2] = q1.y;
+            A1[3, 3] = q1.x;
+            return A1;
+        }
+
+        public static Matrix4x4 randomDistributionOnXY_ZW()
+        {
+            float t1 = Random.value, t2 = Random.value;
+            float c1 = Mathf.Cos(t1), s1 = Mathf.Sin(t1), c2 = Mathf.Cos(t2), s2 = Mathf.Sin(t2);
+            Matrix4x4 A = Matrix4x4.zero;
+            A[0, 0] = c1;
+            A[0, 1] = -s1;
+            A[1, 0] = s1;
+            A[1, 1] = c1;
+            A[2, 2] = c2;
+            A[2, 3] = -s2;
+            A[3, 2] = s2;
+            A[3, 3] = c2;
+
+            return A;
+        }
 
         public static Matrix4x4 randomDistributionOnSO4()
         {
